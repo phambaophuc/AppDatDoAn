c8b1e61eacaa2bedfc1da06d006ed55d7b7c194b
Pham Bao Phuc <123phuc27602@gmail.com>
2023-06-07 21:57:09 +0700

    thêm cửa hàng, sản phẩm

---
+++ src/main/java/Api/AppDatDoAn/entity/CustomUserDetail.java
@@ -0,0 +1,54 @@
+package Api.AppDatDoAn.entity;
+
+import Api.AppDatDoAn.reponsitory.IAccountRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetail implements UserDetails {
+    private final Account account;
+
+    private final IAccountRepository accountRepository;
+
+    public CustomUserDetail(Account account, IAccountRepository accountRepository) {
+        this.account = account;
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
